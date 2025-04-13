b8e09637e21b2b0dbca4580c50f355fbb2ab9352
ramsy0dev <ramsy0dev@gmail.com>
2025-04-13 15:28:46 +0000

    update(nodes.py): add more nodes

--- manv/src/ast/nodes.py
+++ manv/src/ast/nodes.py
@@ -22,12 +22,15 @@
 
 __all__ = [
     "Program",
-    "Number",
-    "Float",
-    "String",
-    "Char",
     "BinaryOp",
     "Identifier",
+    "Return",
+    "Arguments",
+    "Argument",
+    "ArgumentValue",
+    "Function",
+    "CallFunction",
+    "CallConstant",
     "Constant",
     "Assignment"
 ]
@@ -35,41 +38,18 @@ __all__ = [
 from typing import List
 from dataclasses import dataclass, field
 
+# Models
+from manv.models.line_model import LineModel
+
 # Base
 from manv.src.ast.base import ASTNode
+
+# Literals
+from manv.src.builtin.literals import *
 
 # Types
 from manv.src.builtin.types import *
 
-@dataclass
-class Program(ASTNode):
-    statements: List[ASTNode]
-
-    def __init__(self) -> None:
-        self.statements = list()
-    
-    def __repr__(self) -> str:
-        return f"Program(statements={self.statements!r})"
-
-@dataclass
-class Number(ASTNode):
-    value: int
-    typ: Type = field(default_factory=IntType)
-
-@dataclass
-class Float(ASTNode):
-    value: float
-    typ: Type = field(default_factory=FloatType)
-
-@dataclass
-class String(ASTNode):
-    value: str
-    typ: Type = field(default_factory=StrType)
-
-@dataclass
-class Char(ASTNode):
-    value: str
-    typ: Type = field(default_factory=CharType)
 
 @dataclass
 class Identifier(ASTNode):
@@ -78,12 +58,68 @@ class Identifier(ASTNode):
 @dataclass
 class Constant(ASTNode):
     identifier: Identifier
-    size: Number
+    size: NumberLiteral
     typ: Type
     value: ASTNode
+    line: LineModel
 
     def __repr__(self) -> str:
-        return f"Constant(identifier={self.identifier!r}, size={self.size!r}, typ={self.typ!r}, value={self.value!r})"
+        return f"Constant(identifier={self.identifier!r}, size={self.size!r}, typ={self.typ!r}, value={self.value!r}, source_code={self.source_code!r})"
+
+@dataclass
+class CallConstant(ASTNode):
+    identifier: Identifier
+
+@dataclass
+class Variable(ASTNode):
+    identifier: Identifier
+    size: NumberLiteral
+    typ: Type
+    value: ASTNode
+    line: LineModel
+
+@dataclass
+class Return(ASTNode):
+    value: ASTNode
+
+
+@dataclass
+class ArgumentValue(ASTNode):
+    identifier: Identifier
+    value: ASTNode
+    reg_label: str = None   # Used in syscall function
+
+@dataclass
+class Argument(ASTNode):
+    identifier: Identifier
+    typ: Type
+    default_value: ASTNode
+    reg_label: str = None   # Used for syscall functions
+
+@dataclass
+class Arguments(ASTNode):
+    args_list: list[Argument]
+
+@dataclass
+class CallArguments(ASTNode):
+    name: list[ArgumentValue]
+
+@dataclass
+class Function(ASTNode):
+    identifier: Identifier
+    arguments: Arguments
+    statements: List[ASTNode]
+    return_type: Type
+    asm_code: list[str] = None
+    is_syscall: bool = False
+    
+    def __repr__(self) -> str:
+        return f"Function(identifier={self.identifier!r}, arguments={self.arguments!r}, asm_code={self.asm_code!r}, statements={self.statements}, return_type={self.return_type!r}, is_syscall={self.is_syscall!r})"
+
+@dataclass
+class CallFunction(ASTNode):
+    identifier: Identifier
+    args_list: list[ArgumentValue] | None = None
 
 @dataclass
 class BinaryOp(ASTNode):
@@ -93,5 +129,17 @@ class BinaryOp(ASTNode):
 
 @dataclass
 class Assignment(ASTNode):
-    name: str
+    name: StringLiteral
     value: ASTNode
+
+@dataclass
+class Program(ASTNode):
+    statements: List[ASTNode]
+    main: Function
+
+    def __init__(self) -> None:
+        self.statements = list()
+        self.main = None
+    
+    def __repr__(self) -> str:
+        return f"Program(statements={self.statements!r}, main={self.main!r})"
