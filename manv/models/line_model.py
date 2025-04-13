faeb4d782b1f80a7bb80da44f1f43d94191fda9a
ramsy0dev <ramsy0dev@gmail.com>
2025-04-13 15:29:11 +0000

    update(line_model.py): add next_line and last_line attributes

--- manv/models/line_model.py
+++ manv/models/line_model.py
@@ -2,13 +2,22 @@ __all__ = [
     "LineModel"
 ]
 
+class LineModel: ...
+
 class LineModel:
     """
     A line model
     """
-    def __init__(self, content: str, line_number: int) -> None:
+    content: str
+    line_number: int
+    last_line: LineModel | None
+    next_line: LineModel | None
+
+    def __init__(self, content: str, line_number: int, last_line: LineModel | None = None, next_line: LineModel | None = None) -> None:
         self.content = content
         self.line_number = line_number
+        self.last_line = last_line
+        self.next_line = next_line
     
     def __repr__(self):
-        return f"<Line: line_number={self.line_number!r}, content='{self.content!r}'>"
+        return f"LineModel(line_number={self.line_number!r}, content={self.content!r})"
